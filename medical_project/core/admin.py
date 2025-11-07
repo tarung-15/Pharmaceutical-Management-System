43a83536d60219ae891ca4307291663e5dc5072d
tarung-15 <tarungali1526@gmail.com>
2025-11-07 23:25:56 +0530

    Delete medical_project directory

--- medical_project/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Medicine, Bill
-# Register your models here.
-
-admin.site.register(Medicine)
-admin.site.register(Bill)
--- medical_project/core/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_item(dictionary, key):
-    if isinstance(dictionary, dict):
-        return dictionary.get(key, '')
-    return ''
